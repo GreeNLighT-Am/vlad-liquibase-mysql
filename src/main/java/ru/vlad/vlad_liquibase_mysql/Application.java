f396e119ad0818fbae3e0ec6633997b91cbaec31
GreeNLighT-Am <lt38igreen@gmail.com>
2026-05-21 09:30:49 +0300

    Tasks 1 and 2 completed (#1)
    * Tasks 1 and 2 ready
    
    * corrections based on comments from the previous audit
    
    * - убрал лишние переносы строк в мейне
    	- убрал лишние переносы строк в application.yaml
    	- пароль хранится в виде переменной среды DB_PASSWORD
    	- убрал лишние переносы строк pom.xml
    	- удалил избыточную зависимость spring-boot-starter из pom.xml
    	- убрал лишние переносы строк во всех ченджлогмастерах
    	- во всех ченджлогмастерах теперь используется relativeToChangelogFile: true
    	- убрал nullable: false у всех PK
    	- переписал все type: с маленькой
    	- все названия внешнего ключа теперь по образцу: [таблица, в которой fkey]_[таблица, куда он ссылается]_[поле таблицы, куда идёт ссылка]_fkey
    	- все FK теперь с referencedTableName и referencedColumnNames
    	- внёс комментарии к каждой колонке

---
+++ src/main/java/ru/vlad/vlad_liquibase_mysql/Application.java
@@ -0,0 +1,11 @@
+package ru.vlad.vlad_liquibase_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
